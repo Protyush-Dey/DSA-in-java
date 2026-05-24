31dc506b66fdd9ba74bfd261aa767f10240742d5
Protyush-Dey <160174897+Protyush-Dey@users.noreply.github.com>
2026-05-24 19:12:36 +0530

    Implement hasPathSum for binary tree

---
+++ leetcode/q112.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+       return find(root , targetSum) ;
+    }
+    private boolean find(TreeNode root, int t){
+        if(root == null) return false;
+        if (root.left == null && root.right == null) {
+            return root.val == t;
+        }
+        return find(root.left , (t-root.val)) || find(root.right , (t-root.val));
+    }
+
+}
