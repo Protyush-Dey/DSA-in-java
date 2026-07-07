39137d10573ed38f76fe5eee144f8c1f10b15812
Protyush-Dey <160174897+Protyush-Dey@users.noreply.github.com>
2026-07-07 13:04:42 +0530

    Add solution for Codeforces problem 467A

---
+++ CodeForces/q467A.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    int res = 0;
+	    for(int i=0; i<n; i++){
+	        int p = sc.nextInt();
+	        int q = sc.nextInt();
+	        if(q-p>=2) res++;
+	    }
+	    System.out.print(res);
+    }
+}
